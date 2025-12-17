c401678e08a1f7e76653db73274c3e7aeb1661b3
aa0979783096-byte <aa0979783096@gmail.com>
2025-12-17 14:59:00 +0800

    fix: update vite config for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: process.cwd(),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
